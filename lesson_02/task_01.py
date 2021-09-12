d364c6d8285b4a9f018864e2de85873ba4370e22
greenline1 <greenlineqq@gmail.com>
2021-09-11 21:21:35 -0700

    Added exteral argument parsing to the script

--- lesson_02/task_01.py
+++ lesson_02/task_01.py
@@ -1,5 +1,8 @@
+#!/usr/bin/env python3
 '''
-Hometask for lesson2 of QA Automation course
+Hometask for lesson2 of QA Automation course.
+
+>>> usage = 'python3 task_01.py "Some input string for experiments"'
 Part 1
 Tip: use strings methods
 1. Check if a string is number
@@ -15,54 +18,65 @@ and pad it with spaces from the both sides. Display the length of new string.
 but not in the middle of the phrase
 '''
 
-my_string = "123"
-print("Task 1")
-print("Checking if the string is digit...")
-print(my_string.isdigit())
-print("\n")
 
-print("Task 2")
-my_string = "Hi there. How are you?"
-print("Counting spaces in the string...")
-print(my_string.count(" "))
-print("\n")
+def main(argv):
+    input_string = str(argv[1])
 
-print("Task 3")
-print("Counting dots in the string...")
-print(my_string.count("."))
-print("\n")
+    print("Input string is: " + "\"" + input_string + "\"")
+    print("Task 1")
+    print("Checking if the string is digit...")
+    print(input_string.isdigit())
+    print("\n")
 
-print("Task 4")
-print("Padding 'Homework' string with spaces, length = 100")
-task_4 = "Homework"
-task_4_updated = task_4.center(100, " ")
-print("updated string:\n" + task_4_updated)
-print("The length of updated string is " + str(len(task_4_updated)))
+    print("Task 2")
+    print("Counting spaces in the string...")
+    print(input_string.count(" "))
+    print("\n")
 
-print("Task 5")
-print("Modifying string to UPPERCASE")
-print(my_string.upper())
-print("\n")
+    print("Task 3")
+    print("Counting dots in the string...")
+    print(input_string.count("."))
+    print("\n")
 
-print("Task 6")
-print("Checking if the string ends with 'ing'...")
-print(my_string.endswith("ing"))
-print("\n")
+    print("Task 4")
+    print("Padding 'Homework' string with spaces, length = 100")
+    task_4 = "Homework"
+    task_4_updated = task_4.center(100, " ")
+    print("updated string:\n" + task_4_updated)
+    print("The length of updated string is " + str(len(task_4_updated)))
+    print("\n")
 
-print("Task 7")
-print("Searching the index of first occurrence of \"a\" character in the string \"" + my_string + "\"...")
-print(my_string.find("a"))
-print("\n")
+    print("Task 5")
+    print("Modifying string to UPPERCASE")
+    print(input_string.upper())
+    print("\n")
 
-print("Task 8")
-print("Dividing string on the two subsrings by space symbol...")
-print(my_string.split(" "))
-print("\n")
+    print("Task 6")
+    print("Checking if the string ends with 'ing'...")
+    print(input_string.endswith("ing"))
+    print("\n")
 
-print("Task 9")
-print("New string for this task: \"" + " "*10 + my_string + " "*8 + "\"")
-print("Removing spaces on the beginning and and of the string...")
-print(my_string.strip(" "))
-print("\n")
+    print("Task 7")
+    print("Searching the index of first occurrence of \"a\" character in the string \"" + input_string + "\"...")
+    print(input_string.find("a"))
+    print("\n")
+
+    print("Task 8")
+    print("Dividing string on the list of subsrings by space symbol...")
+    print(input_string.split(" "))
+    print("\n")
+
+    print("Task 9")
+    print("New string for this task: \"" + " " * 10 + input_string + " " * 8 + "\"")
+    print("Removing spaces on the beginning and and of the string...")
+    print(input_string.strip(" "))
+    print("\n")
 
 
+if __name__ == '__main__':
+    def _script_io():
+        from sys import argv
+
+        main(argv)
+
+    _script_io()
