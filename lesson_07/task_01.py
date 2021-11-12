ecd24c27ab13b06e370f06103dcf2e2a0c512a9e
greenline1 <greenlineqq@gmail.com>
2021-11-11 19:06:39 -0800

    Update test for task_01

--- lesson_07/task_01.py
+++ lesson_07/task_01.py
@@ -1,6 +1,6 @@
 import unittest
 from lesson_03.task_03 import is_year_leap
-from lesson_03.task_04 import is_triangle
+from lesson_03.task_04 import is_triangle, find_triangle_type
 
 
 class TestLesson03(unittest.TestCase):
@@ -28,6 +28,30 @@ class TestLesson03(unittest.TestCase):
     def test_triangle_03(self):
         sides = (0, 0, 0)
         self.assertFalse(is_triangle(*sides))
+
+    def test_triangle_04(self):
+        sides = ('a', 'b', 'c')
+        self.assertFalse(is_triangle(*sides))
+
+    def test_triangle_type_01(self):
+        sides = (3, 4, 5)
+        self.assertEqual(find_triangle_type(*sides), "Versatile triangle")
+
+    def test_triangle_type_02(self):
+        sides = (5, 5, 5)
+        self.assertEqual(find_triangle_type(*sides), "Equilateral triangle")
+
+    def test_triangle_type_03(self):
+        sides = (5, 5, 3)
+        self.assertEqual(find_triangle_type(*sides), "Isosceles triangle")
+
+    def test_triangle_type_04(self):
+        sides = (0, 0, 0)
+        self.assertEqual(find_triangle_type(*sides), "Not a triangle")
+
+    def test_triangle_type_05(self):
+        sides = ("a", "b", "c")
+        self.assertEqual(find_triangle_type(*sides), "Not a triangle")
 
 
 if __name__ == '__main__':
