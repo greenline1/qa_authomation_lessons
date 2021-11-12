314d9b7db1f0565e5a8508503aac2017a954477c
greenline1 <greenlineqq@gmail.com>
2021-11-11 16:54:41 -0800

    Add function that is checking for a triangle type

--- lesson_03/task_04.py
+++ lesson_03/task_04.py
@@ -1,19 +1,36 @@
 def is_triangle(a: float, b: float, c: float) -> bool:
     """Check if numbers a, b, c are sides of a triangle"""
-    verify_triangle_sides(a, b, c)
-    if a + b > c and a + c > b and c + b > a:
-        return True
-    else:
+    if not verify_triangle_sides(a, b, c):
         return False
+    else:
+        print("ok")
+        a, b, c = float(a), float(b), float(c)
+        if a + b > c and a + c > b and c + b > a:
+            return True
+        else:
+            return False
 
 
-def verify_triangle_sides(a: float, b: float, c: float) -> None:
+def verify_triangle_sides(a: float, b: float, c: float) -> bool:
     """Verify if triangle sides are floats"""
     try:
         a, b, c = float(side_1), float(side_2), float(side_3)
-    except ValueError as e:
-        print("Incorrect input of the triangle sides\n", e, sep="")
-        exit(-1)
+        return True
+    except ValueError:
+        print("Incorrect input of the triangle sides\n", sep="")
+    return False
+
+
+def find_triangle_type(a, b, c):
+    """Make additional checks of triangle type"""
+    if not is_triangle(a, b, c):
+        return "Not a triangle"
+    elif a == b == c:
+        return "Equilateral triangle"
+    elif a == b or b == c or a==c:
+        return "Isosceles triangle"
+    else:
+        return "Versatile triangle"
 
 
 if __name__ == '__main__':
@@ -22,7 +39,11 @@ if __name__ == '__main__':
     side_2 = input("b=")
     side_3 = input("c=")
 
-    if is_triangle(side_1, side_2, side_3):
+    sides = [side_1, side_2, side_3]
+
+    if is_triangle(*sides):
         print("Yes")
     else:
         print("No")
+
+    print(find_triangle_type(*sides))
